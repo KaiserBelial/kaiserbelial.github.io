310e50704906c60dd1d0815a685f5e9324ff4fea
Kaiserbelial <152095857+Kaiserbelial@users.noreply.github.com>
2026-02-27 11:25:33 +0000

    Contact form and styling

--- main.js
+++ main.js
@@ -5,8 +5,57 @@ projects.forEach(project => {
         <img src="${project.image}" alt="${project.title}">
         <div class="project-info">
             <h3>${project.title}</h3>
-            <p>${project.description}</p>
+            <p>${project.shortDescription}</p>
         </div>
     </a>
   `
 })
+
+emailjs.init('ETXvpDdmqa-i3Q_yE')
+
+function sendMessage() {
+  const name = document.getElementById('name').value
+  const email = document.getElementById('email').value
+  const message = document.getElementById('message').value
+  const title = document.getElementById('title').value
+  const status = document.getElementById('form-status')
+
+  if (name.trim() === '') {
+    status.textContent = 'Please enter your name'
+    return
+  }
+
+  if (email.trim() === '' || !email.includes('@') || !email.includes('.co')){
+    status.textContent = 'Please enter a valid email'
+    return
+  }
+
+  if (title.trim() === '') {
+    status.textContent = 'Please enter a subject'
+    return
+  }
+
+  if (message.trim() === '') {
+    status.textContent = 'Please enter a message'
+    return
+  }
+
+  emailjs.send('kaiser_belial', 'contact_template', {
+    from_name: name,
+    from_email: email,
+    message: message,
+    title: title
+  })
+  .then(() =>{
+    status.textContent = 'Message sent!'
+    document.getElementById('name').value = ''
+    document.getElementById('email').value = ''
+    document.getElementById('title').value = ''
+    document.getElementById('message').value = ''
+    document.getElementById('send-btn').disabled = true
+    
+  })
+  .catch(() =>{
+    status.textContent = 'Something went wrong'
+  })
+}
--- projects.js
+++ projects.js
@@ -1,6 +1,10 @@
 /*
         title: ,
-        description: ,
+        shortDescription: ,
+        fullDescription: `
+        <p>
+        </p>
+        `,
         image: ,
         page: ,
         link: 
@@ -9,7 +13,29 @@
 var projects = [
     {
         title: "Masquerade Murderer",
-        description: "My first ever Game Jam made using Godot.",
+        shortDescription: "My first ever Game Jam made using Godot.",
+        fullDescription: `
+        <p>
+        I participated in the Global Game Jam 2026 at Surrey 
+        where developers from different disciplines came together to create a 
+        game within a limited time (48 hrs). The theme for this year's jam was 
+        "Masks", which engouraged teams to explore ideas about identity, 
+        deception and hidden intentions.
+        </p>
+        <p>
+        During the jam, my team created a 2D top-down action game called 
+        Masquerade Murderer using the Godot engine. The core gameplay ocuses on 
+        stealth and strategy. Players must move through the party and kill 
+        without alerting the guards attention. Killing different masked npcs 
+        gave you different powers (dash ability, further reach, speed increase etc) 
+
+        This project was a fast-paced collaborative experience where we designed 
+        mechanics, implemented gameplay systems, and build a playable game within a 
+        short gam jam timeframe. It was a great opportunity to practice teamwork, 
+        rapid prototyping, and rapid learning on the job (None of us had previous 
+        experience in Godot).
+        </p>
+        `,
         image: "/img/masquerade_murderer_gamejam_thumbnail.png",
         page: "projects/masquerade_murderer",
         link: "https://globalgamejam.org/games/2026/masquerade-murderer-9"
@@ -17,14 +43,55 @@ var projects = [
     },
     {
         title: "Placeholder",
-        description: "Placeholder",
+        shortDescription: "Placeholder",
+        fullDescription: `
+        <p>
+        </p>
+        `,
         image: "/img/masquerade_murderer_gamejam_thumbnail.png",
         page: "projects/masquerade_murderer",
         link: "https://globalgamejam.org/games/2026/masquerade-murderer-9"
     },
     {
         title: "Placeholder",
-        description: "Placeholder",
+        shortDescription: "Placeholder",
+        fullDescription: `
+        <p>
+        </p>
+        `,
+        image: "/img/masquerade_murderer_gamejam_thumbnail.png",
+        page: "projects/masquerade_murderer",
+        link: "https://globalgamejam.org/games/2026/masquerade-murderer-9"
+    },
+    {
+        title: "Placeholder",
+        shortDescription: "Placeholder",
+        fullDescription: `
+        <p>
+        </p>
+        `,
+        image: "/img/masquerade_murderer_gamejam_thumbnail.png",
+        page: "projects/masquerade_murderer",
+        link: "https://globalgamejam.org/games/2026/masquerade-murderer-9"
+    },
+    {
+        title: "Placeholder",
+        shortDescription: "Placeholder",
+        fullDescription: `
+        <p>
+        </p>
+        `,
+        image: "/img/masquerade_murderer_gamejam_thumbnail.png",
+        page: "projects/masquerade_murderer",
+        link: "https://globalgamejam.org/games/2026/masquerade-murderer-9"
+    },
+    {
+        title: "Placeholder",
+        shortDescription: "Placeholder",
+        fullDescription: `
+        <p>
+        </p>
+        `,
         image: "/img/masquerade_murderer_gamejam_thumbnail.png",
         page: "projects/masquerade_murderer",
         link: "https://globalgamejam.org/games/2026/masquerade-murderer-9"
@@ -34,11 +101,13 @@ var projects = [
 const path = window.location.pathname
 const project = projects.find(p => path.includes(p.page))
 
-document.querySelector('#project-content').innerHTML =`
-    <a href="/">< Back</a>
+if (document.querySelector('#project-content') && project) {
+  document.querySelector('#project-content').innerHTML = `
+    <a href="/" class="back">< Back</a>
     <h1>${project.title}</h1>
     <img src="${project.image}" alt="${project.title}">
-    <p>${project.description}</p>
-    <a href="${project.link}" target="_blank">View project</a>
-`
-document.title = project.title
+    ${project.fullDescription}
+    <a href="${project.link}" class="view" target="_blank">View project</a>
+  `
+  document.title = project.title
+}
